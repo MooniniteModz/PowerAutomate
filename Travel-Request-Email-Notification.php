17c78629ee306863f04a3d48fc5464bbddd38eb6
Moon <MooniniteModz@proton.me>
2025-05-28 18:38:09 -0500

    Init Commit

---
+++ Travel-Request-Email-Notification.php
@@ -0,0 +1,86 @@
+
+
+Travel Request Form Submitted
+
+Employee Name: @{outputs('Travel_Form_Answers')?['body/r7296fb5732d44b218fa3de9c8268f6cd']} @{outputs('Travel_Form_Answers')?['body/rae5e555966aa49c1aab7d02472436e08']} @{outputs('Travel_Form_Answers')?['body/rd24d311560374eac95841993e6d4e8c1']}
+Mobile Number: @{outputs('Travel_Form_Answers')?['body/rece0824db0aa496d84238b3307d9d998']}
+E-Mail: @{outputs('Travel_Form_Answers')?['body/r31c0dd984061408a8a7f6717f21c66ac']}
+
+Company: @{outputs('Travel_Form_Answers')?['body/rccf76def1cc24ea0abf7de74963e7946']}
+Site: @{outputs('Travel_Form_Answers')?['body/rcad66b27077b4a74875bfff068f8c4bb']}@{outputs('Travel_Form_Answers')?['body/r9b17247fe7cc45edb194c74362416ea5']}
+Department: @{outputs('Travel_Form_Answers')?['body/rb399d06849b24392b83c2e3d3b7491e7']}@{outputs('Travel_Form_Answers')?['body/rf8bbd31120eb4748bc89a3efe0fd84da']}
+
+Guest Type: @{outputs('Travel_Form_Answers')?['body/red4b609a8b8a428d986d707fa0ae07e2']}@{outputs('Travel_Form_Answers')?['body/re9cea3469d374a78a8a325194c88a367']}
+
+Reason for travel: @{outputs('Travel_Form_Answers')?['body/r9b9bf7fb164d44bba39a6cd61b65725b']}@{outputs('Travel_Form_Answers')?['body/r9826587859f14c20818aa6c519c9d54f']}
+Submitted By: @{outputs('Travel_Form_Answers')?['body/rf7e08e8ff6b54bc8975192287425aaae']}@{outputs('Travel_Form_Answers')?['body/recaf776883d34c7f854bef4a56bd8c5e']}
+Manager: @{outputs('Travel_Form_Answers')?['body/r0e7fe5daae0048f780b94b1a0a21018e']}@{outputs('Travel_Form_Answers')?['body/r97fa337944e840d2b7d9dacb460005b8']}
+
+
+
+Air Travel : @{outputs('Travel_Form_Answers')?['body/r0abbaa199d1944a2bb19e8c7e950358c']}@{outputs('Travel_Form_Answers')?['body/r1da084172fa34b7db9bc945b86ba5906']}
+
+Flight : @{outputs('Travel_Form_Answers')?['body/rcb48e1b9470b4a979532d5978bd9bebf']}
+Desired Airline: @{outputs('Travel_Form_Answers')?['body/ra0656872d50a49e0afefb60df6479250']}
+Give Detailed Information For a Multi-City Flight: @{outputs('Travel_Form_Answers')?['body/rdafb6065a5c346ec811c110c3c88db63']}
+From (City): @{outputs('Travel_Form_Answers')?['body/rde2dc08a9efd49c99bbc681180f1a147']}
+To (City): @{outputs('Travel_Form_Answers')?['body/r7d39835d1c664b7ea10414c6d3b96657']}
+Departure Date: @{outputs('Travel_Form_Answers')?['body/rc21041b4faf24eeca329ab9d9dabe0d1']}
+Return Date: @{outputs('Travel_Form_Answers')?['body/raf71561f611f4384b69a64a1f43b90aa']}
+Departure Time (Actual Time or General Time of Day): @{body('Travel_Form_Answers')?['r5e62303b5a44439b9945aed35663bc9e']}
+Return Time (Actual Time or General Time of Day): @{outputs('Travel_Form_Answers')?['body/r92a5542bce134e40814337d59fc53b38']}
+
+Employee Name: @{outputs('Travel_Form_Answers')?['body/r7296fb5732d44b218fa3de9c8268f6cd']} @{outputs('Travel_Form_Answers')?['body/rae5e555966aa49c1aab7d02472436e08']} @{outputs('Travel_Form_Answers')?['body/rd24d311560374eac95841993e6d4e8c1']}
+Date of Birth: @{outputs('Travel_Form_Answers')?['body/r76bc038cfced48f1a4536921b6928f4c']}
+Gender: @{outputs('Travel_Form_Answers')?['body/r56113d1f5d30454b98dff931cb0bc6d9']}
+Airline Rewards Number: @{outputs('Travel_Form_Answers')?['body/r374465a1eaed4e9fa1bb151a05f9f499']}
+Known Traveler Number: @{outputs('Travel_Form_Answers')?['body/r8aed685dd6724837ac00b1624a2d7328']}
+Mobile Number: @{outputs('Travel_Form_Answers')?['body/rece0824db0aa496d84238b3307d9d998']}
+E-Mail: @{outputs('Travel_Form_Answers')?['body/r31c0dd984061408a8a7f6717f21c66ac']}
+
+
+Desired Seat: @{body('Travel_Form_Answers')?['r0d3cd7f6787b4185b94b00602f85fa70']}
+
+
+Miscellaneous Information: @{outputs('Travel_Form_Answers')?['body/rbe5d5deaaee14e099c3866ef63d2e0fb']}
+
+
+Personal Transportation: @{outputs('Travel_Form_Answers')?['body/r3f4fa0026ca84494b00f439d1dc76bd4']}
+
+Reservation Needs: @{outputs('Travel_Form_Answers')?['body/r92551c71d4494a92a0207e9f04c54a24']} Rental Pick Up City / Airport: @{outputs('Travel_Form_Answers')?['body/rcadb53bc56544e2eab9810c7a2824c22']}
+Rental Pick Up Date: @{outputs('Travel_Form_Answers')?['body/ra54742005f5441c9bb9175a7425c880d']}
+Rental Pick Up Time(Include AM/PM): @{outputs('Travel_Form_Answers')?['body/ra116bf5675de4f21abe49608910fadc4']}
+
+
+Rental Drop Off City / Airport: @{outputs('Travel_Form_Answers')?['body/rafcfcc75ff804086bd96042db6aac534']}
+Rental Return Date: @{outputs('Travel_Form_Answers')?['body/rcfb4e8edb29042bfb197ffec39a5e7fe']}
+Rental Return Time(Include AM/PM):@{outputs('Travel_Form_Answers')?['body/r92a5542bce134e40814337d59fc53b38']}
+
+Employee Name: @{outputs('Travel_Form_Answers')?['body/r7296fb5732d44b218fa3de9c8268f6cd']} @{outputs('Travel_Form_Answers')?['body/rae5e555966aa49c1aab7d02472436e08']} @{outputs('Travel_Form_Answers')?['body/rd24d311560374eac95841993e6d4e8c1']}
+Mobile Number: @{outputs('Travel_Form_Answers')?['body/rece0824db0aa496d84238b3307d9d998']}
+E-Mail: @{outputs('Travel_Form_Answers')?['body/r31c0dd984061408a8a7f6717f21c66ac']}
+
+Miscellaneous Information Regarding Automotive Travel: @{outputs('Travel_Form_Answers')?['body/r42bc1b5079b64033afa20b4752405267']}
+
+
+
+Lodging: @{outputs('Travel_Form_Answers')?['body/rf3db127b1b3645f5a17247fad542d51b']}
+
+City & State: @{outputs('Travel_Form_Answers')?['body/r04a1c87f923247a5a807b739122190f6']}
+Specific Hotel and Address if Known: @{outputs('Travel_Form_Answers')?['body/r516a4bfe85074308be676e4f093db891']}
+Hotel rewards number: @{outputs('Travel_Form_Answers')?['body/r762092e79aec435e967208987f426b2e']}
+Check In Date: @{outputs('Travel_Form_Answers')?['body/r55f57a47a95640af92e2657f1eaadf1a']}
+Check Out Date: @{outputs('Travel_Form_Answers')?['body/r0663b3d7ffa0474983248cf5ac6ab6e0']}
+Truck Parking Required: @{outputs('Travel_Form_Answers')?['body/r1f0208061042493791443971b28b6e22']}
+Bed Type: @{outputs('Travel_Form_Answers')?['body/r7efaa9e449d04eb1aad1106cdd8238d6']}
+
+Employee Name: @{outputs('Travel_Form_Answers')?['body/r7296fb5732d44b218fa3de9c8268f6cd']} @{outputs('Travel_Form_Answers')?['body/rae5e555966aa49c1aab7d02472436e08']} @{outputs('Travel_Form_Answers')?['body/rd24d311560374eac95841993e6d4e8c1']}
+Mobile Number: @{outputs('Travel_Form_Answers')?['body/rece0824db0aa496d84238b3307d9d998']}
+E-Mail: @{outputs('Travel_Form_Answers')?['body/r31c0dd984061408a8a7f6717f21c66ac']}
+Hotel Rewards Number: @{outputs('Travel_Form_Answers')?['body/r762092e79aec435e967208987f426b2e']}
+Company: @{outputs('Travel_Form_Answers')?['body/rccf76def1cc24ea0abf7de74963e7946']}
+Site: @{outputs('Travel_Form_Answers')?['body/rcad66b27077b4a74875bfff068f8c4bb']}@{outputs('Travel_Form_Answers')?['body/r9b17247fe7cc45edb194c74362416ea5']}
+Department: @{outputs('Travel_Form_Answers')?['body/rb399d06849b24392b83c2e3d3b7491e7']}@{outputs('Travel_Form_Answers')?['body/rf8bbd31120eb4748bc89a3efe0fd84da']}
+Reason For Travel: @{outputs('Travel_Form_Answers')?['body/r9b9bf7fb164d44bba39a6cd61b65725b']}
+
+Miscellaneous Information Regarding Hotel Stay:@{outputs('Travel_Form_Answers')?['body/rabf276013fb84d85bc829a2022dbf5ad']}
